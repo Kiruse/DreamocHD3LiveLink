444814baa399631f7a34d4dd9c1a1c5a0a2eb4b9
Kiruse <neurokiruse@outlook.com>
2020-03-08 16:42:25 +0100

    Made usage easier
    Now can simply call Profiler.segment() method to insert a new subsegment
    into currently active segment, or create a new
    root segment if none currently active.

--- profiler.py
+++ profiler.py
@@ -10,32 +10,44 @@ class Profiler:
     def __init__(self):
         # Collection of ProfilerSegment instances
         self.segments = []
+        
+        # Track currently active segment so user code can simply call `segment` method to properly insert
+        # a new segment.
+        self.active_segment = None
     
     def segment(self, name = "<unnamed segment>"):
-        segment = ProfilerSegment(name)
-        self.segments.append(segment)
-        return segment
+        if self.active_segment is None:
+            segment = ProfilerSegment(self, name)
+            self.segments.append(segment)
+            return segment
+        else:
+            return self.active_segment.segment(name)
     
     def dump(self, io = sys.stdout):
         for segment in self.segments:
             segment.dump(0, io)
 
 class ProfilerSegment:
-    def __init__(self, name = "<unnamed segment>"):
+    def __init__(self, profiler, name = "<unnamed segment>"):
+        self.profiler = profiler
         self.name = name
         self.subsegments = []
         self._start = -1 # time.perf_counter_ns() does not have a guaranteed reference point, so knowing start and end times is useless.
         self.diff   = -1
+        self._prev_active = None
     
     def __enter__(self):
+        self._prev_active = self.profiler.active_segment
+        self.profiler.active_segment = self
         self._start = perf_counter_ns()
         return self
     
     def __exit__(self, *args, **kwargs):
         self.diff = perf_counter_ns() - self._start
+        self.profiler.active_segment = self._prev_active
     
     def segment(self, name = "<unnamed segment>"):
-        segment = ProfilerSegment(name)
+        segment = ProfilerSegment(self.profiler, name)
         self.subsegments.append(segment)
         return segment
     
